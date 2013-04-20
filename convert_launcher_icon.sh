eb56bf89d3544115005fc6978b1b495add9cfa25
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2013-04-20 12:31:03 +0200

    Prepared app icon and name for Playstore launch.

---
+++ convert_launcher_icon.sh
@@ -0,0 +1,5 @@
+convert playstore/app_icon.png -resize 512x512 icon.png
+convert playstore/app_icon.png -resize 96x96 res/drawable-xhdpi/ic_launcher.png
+convert playstore/app_icon.png -resize 72x72 res/drawable-hdpi/ic_launcher.png
+convert playstore/app_icon.png -resize 48x48 res/drawable-mdpi/ic_launcher.png
+convert playstore/app_icon.png -resize 36x36 res/drawable-ldpi/ic_launcher.png
